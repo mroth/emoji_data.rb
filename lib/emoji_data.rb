fc7d703c2261371bcc58d65181c2ce0160b7afa3
Matthew Rothenberg <mrothenberg@gmail.com>
2013-06-07 14:22:51 -0400

    rename methods to make more sense

--- lib/emoji_data.rb
+++ lib/emoji_data.rb
@@ -15,15 +15,22 @@ module EmojiData
     @codepoints ||= EMOJI_MAP.map { |es| es['unified'] }
   end
 
-  def self.char_to_codepoint(char)
+  def self.char_to_unified(char)
     char.codepoints.to_a.map {|i| i.to_s(16).rjust(4,'0')}.join('-').upcase
   end
 
-  def self.codepoint_to_char(cp)
-    find_by_codepoint(cp).to_char
+  def self.unified_to_char(cp)
+    find_by_unified(cp).to_char
   end
 
-  def self.find_by_codepoint(cp)
+  def self.find_by_unified(cp)
     EMOJI_CHARS.detect { |ec| ec.unified == cp }
   end
+
+  # not sure why the below doesnt work... but maybe fuck reverse compatibility for MYSELF
+  # self.module_eval do
+  #   alias_method :find_by_codepoint, :find_by_unified
+  #   alias_method :char_to_codepoint, :char_to_unified
+  #   alias_method :codepoint_to_char, :unified_to_char
+  # end
 end
--- spec/emoji_data_spec.rb
+++ spec/emoji_data_spec.rb
@@ -2,25 +2,25 @@
 require 'spec_helper'
 
 describe EmojiData do
-  describe "#char_to_codepoint" do
+  describe "#char_to_unified" do
     it "converts normal emoji to unified codepoint" do
-      EmojiData.char_to_codepoint("👾").should eq('1F47E')
-      EmojiData.char_to_codepoint("🚀").should eq('1F680')
+      EmojiData.char_to_unified("👾").should eq('1F47E')
+      EmojiData.char_to_unified("🚀").should eq('1F680')
     end
     it "converts double-byte emoji to proper codepoint" do
-      EmojiData.char_to_codepoint("🇺🇸").should eq('1F1FA-1F1F8')
-      EmojiData.char_to_codepoint("#⃣").should eq('0023-20E3')
+      EmojiData.char_to_unified("🇺🇸").should eq('1F1FA-1F1F8')
+      EmojiData.char_to_unified("#⃣").should eq('0023-20E3')
     end
   end
 
-  describe "#codepoint_to_char" do
+  describe "#unified_to_char" do
     it "converts normal unified codepoints to unicode strings" do
-      EmojiData.codepoint_to_char('1F47E').should eq("👾")
-      EmojiData.codepoint_to_char('1F680').should eq("🚀")
+      EmojiData.unified_to_char('1F47E').should eq("👾")
+      EmojiData.unified_to_char('1F680').should eq("🚀")
     end
     it "converts doublebyte unified codepoints to unicode strings" do
-      EmojiData.codepoint_to_char('1F1FA-1F1F8').should eq("🇺🇸")
-      EmojiData.codepoint_to_char('0023-20E3').should eq("#⃣")
+      EmojiData.unified_to_char('1F1FA-1F1F8').should eq("🇺🇸")
+      EmojiData.unified_to_char('0023-20E3').should eq("#⃣")
     end
   end
 end
