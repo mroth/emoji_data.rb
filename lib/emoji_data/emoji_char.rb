05670927f6e7d84c1d8c301b47888ea4614f1892
Matthew Rothenberg <mrothenberg@gmail.com>
2013-06-07 15:01:04 -0400

    dynamically allocate instance vars and reader

--- lib/emoji_data/emoji_char.rb
+++ lib/emoji_data/emoji_char.rb
@@ -1,20 +1,13 @@
 module EmojiData
 
   class EmojiChar
-    attr_reader :name, :unified, :short_name
-
     def initialize(emoji_hash)
-      @name       = emoji_hash['name']
-      @unified    = emoji_hash['unified']
-      @docomo     = emoji_hash['docomo']
-      @au         = emoji_hash['au']
-      @softbank   = emoji_hash['softbank']
-      @google     = emoji_hash['google']
-      @image      = emoji_hash['image']
-      @sheet_x    = emoji_hash['sheet_x']
-      @sheet_y    = emoji_hash['sheet_y']
-      @short_name = emoji_hash['short_name']
-      @text       = emoji_hash['text']
+      # http://stackoverflow.com/questions/1615190/declaring-instance-variables-iterating-over-a-hash
+      emoji_hash.each do |k,v|
+        instance_variable_set("@#{k}",v)
+        eigenclass = class<<self; self; end
+        eigenclass.class_eval { attr_reader k }
+      end
     end
 
     def to_char
